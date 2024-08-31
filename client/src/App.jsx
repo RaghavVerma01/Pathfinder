eefabd28b4a647f546fbeee91754978b1e28f936
devv1002 <devmalik1002@gmail.com>
2024-08-31 20:01:13 +0530

    Landing Page configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Button } from "./components/ui/button"
 import './index.css'
 import './App.css';
 import Header from "./components/util/Header";
-import Home from "./components/util/Home";
+import Home from './components/util/Home';
 function App() {
 
 
